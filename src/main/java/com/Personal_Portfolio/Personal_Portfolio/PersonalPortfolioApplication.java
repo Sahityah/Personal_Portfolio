b8bea820b9e710aed66c213bb9e68cb28fe7e743
MALATESHMKELUR <kelurmalatesh@gmail.com>
2025-06-21 17:31:52 +0530

    🚀 latest changes for deploy

--- src/main/java/com/Personal_Portfolio/Personal_Portfolio/PersonalPortfolioApplication.java
+++ src/main/java/com/Personal_Portfolio/Personal_Portfolio/PersonalPortfolioApplication.java
@@ -4,22 +4,12 @@ import com.Personal_Portfolio.Personal_Portfolio.Config.GoogleOAuthConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GoogleOAuthConfig.class)
-
 public class PersonalPortfolioApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-
 		SpringApplication.run(PersonalPortfolioApplication.class, args);
 	}
 
